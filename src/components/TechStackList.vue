d11a7d19141f59c4802de9845f3cd231ac1dd8be
Lehoczky Zoltán <ifjlehoczkyzoltan@gmail.com>
2024-06-05 22:51:57 +0200

    add tech stack

---
+++ src/components/TechStackList.vue
@@ -0,0 +1,18 @@
+<template>
+  <ul
+    class="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6"
+  >
+    <TechStackListItem
+      v-for="(tech, i) in techs"
+      v-bind="{ ...tech, index: i }"
+    ></TechStackListItem>
+  </ul>
+</template>
+
+<script setup lang="ts">
+import TechStackListItem from "./TechStackListItem.vue"
+
+defineProps<{
+  techs: Array<{ href: string; icon: string; name: string }>
+}>()
+</script>
---
+++ src/components/TechStackListItem.vue
@@ -0,0 +1,43 @@
+<template>
+  <li class="list-item opacity-0">
+    <a
+      :href="href"
+      class="flex flex-col items-center justify-center gap-4 rounded-2xl border-2 border-slate-800 p-10 shadow-xl transition-colors duration-200 hover:border-green-500 hover:text-green-500"
+    >
+      <div class="text-[5rem]" :class="icon"></div>
+      <div class="text-xl">{{ name }}</div>
+    </a>
+    <div class="animated-border-box-glow -z-10"></div>
+  </li>
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue"
+
+const props = defineProps<{
+  href: string
+  name: string
+  icon: string
+  index: number
+}>()
+
+const delay = computed(() => `${props.index * 50}ms`)
+</script>
+
+<style scoped>
+.list-item {
+  animation: appear 300ms ease-in forwards;
+  animation-delay: v-bind(delay);
+}
+
+@keyframes appear {
+  from {
+    transform: translateY(2rem);
+  }
+
+  to {
+    opacity: 1;
+    transform: translateY(0rem);
+  }
+}
+</style>
