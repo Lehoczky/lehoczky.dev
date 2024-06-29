e3a18c5b90ea36e8c784b112621253b4a5faa2e2
Lehoczky Zoltán <ifjlehoczkyzoltan@gmail.com>
2024-06-29 20:08:46 +0200

    add hover effect to passions

--- src/components/Passions/PassionCard.vue
+++ src/components/Passions/PassionCard.vue
@@ -1,5 +1,17 @@
 <template>
-  <div class="flex gap-8 rounded-2xl border-2 border-neutral-800 p-10">
-    <slot></slot>
+  <div
+    class="card group relative isolate grid overflow-hidden rounded-2xl bg-neutral-700 bg-opacity-20 p-0.5"
+  >
+    <div
+      class="inner flex gap-8 rounded-2xl bg-neutral-800 p-10 transition-all duration-300 ease-in-out group-hover:bg-opacity-60 group-hover:backdrop-blur-3xl"
+    >
+      <slot></slot>
+    </div>
+    <div
+      class="blob absolute left-0 top-0 -z-10 size-64 rounded-full bg-neutral-100 bg-opacity-50 opacity-0 blur-2xl transition-all duration-300 ease-in-out"
+    ></div>
+    <div
+      class="fake-blob invisible absolute left-0 top-0 -z-10 size-52 rounded-full"
+    ></div>
   </div>
 </template>
