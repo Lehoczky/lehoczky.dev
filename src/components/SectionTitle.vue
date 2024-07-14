18f9fb76c99c615641550e2b2f9363ae6004d4e1
Lehoczky Zoltán <ifjlehoczkyzoltan@gmail.com>
2024-07-14 21:01:40 +0200

    animate section titles

--- src/components/SectionTitle.vue
+++ src/components/SectionTitle.vue
@@ -1,7 +1,13 @@
 <template>
   <h2
-    class="mb-10 scroll-m-24 text-5xl tracking-tighter text-green-500 lg:mb-12 xl:mb-16"
+    class="mb-10 scroll-m-24 text-5xl tracking-tighter text-green-500 transition-[letter-spacing,opacity] duration-500 lg:mb-12 xl:mb-16"
   >
     <slot></slot>
   </h2>
 </template>
+
+<style lang="css" scoped>
+.out {
+  @apply !tracking-normal opacity-40;
+}
+</style>
