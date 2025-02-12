4174ecf9a6912b5e6167dcb31549c8852a9f2899
Lehoczky Zoltán <ifjlehoczkyzoltan@gmail.com>
2025-02-12 19:50:09 +0100

    add schema.org data

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,1 +1,4 @@
+export const MY_GITHUB = "https://github.com/Lehoczky"
+export const MY_LINKEDIN =
+  "https://www.linkedin.com/in/zolt%C3%A1n-lehoczky-bb32aa188/"
 export const bodyScrolledObserverElementId = "body-scrolled-observer-element"
