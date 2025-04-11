0ca076794395ad5fda894dec528a4dcd4603d05b
Lehoczky Zoltán <ifjlehoczkyzoltan@gmail.com>
2025-04-11 16:08:39 +0200

    remove the usage of "apply" directives

--- src/modules/shared/components/SectionTitle.vue
+++ src/modules/shared/components/SectionTitle.vue
@@ -1,13 +1,7 @@
 <template>
   <h2
-    class="mb-10 inline-block scroll-m-24 bg-gradient-to-r from-green-500 to-green-400 bg-clip-text text-5xl tracking-tighter text-transparent transition-[letter-spacing,opacity] duration-500 lg:mb-12 xl:mb-16"
+    class="mb-10 inline-block scroll-m-24 bg-gradient-to-r from-green-500 to-green-400 bg-clip-text text-5xl tracking-tighter text-transparent transition-[letter-spacing,opacity] duration-500 data-[out]:tracking-normal data-[out]:opacity-40 lg:mb-12 xl:mb-16"
   >
     <slot></slot>
   </h2>
 </template>
-
-<style lang="css" scoped>
-.out {
-  @apply !tracking-normal opacity-40;
-}
-</style>
